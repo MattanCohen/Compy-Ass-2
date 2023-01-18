a773e388086a7d2f9b75e1fe05d243ee967ee1a3
nadavh <nadavhb@gmail.com>
2023-01-18 02:59:09 +0200

    ScmApplic' (proc, args, Non_Tail_Call)

--- src/pc_and_compiler.ml
+++ src/pc_and_compiler.ml
@@ -1642,7 +1642,6 @@ module type CODE_GENERATION =
   end;;
 
 
-
 module Code_Generation : CODE_GENERATION = struct
 
   (* areas that raise this exception are NOT for the
@@ -1678,6 +1677,9 @@ module Code_Generation : CODE_GENERATION = struct
   
   let foreach: ('a -> unit) -> 'a list -> unit = fun func list ->
     List.fold_left (fun _ x -> func x) () list;;
+  let range n =
+    let rec run i = if i = n then [] else i :: run (i+1) in
+    run 0
 
   let remove_duplicates list =
     let res =  ref [] in
@@ -2172,9 +2174,18 @@ module Code_Generation : CODE_GENERATION = struct
          ^ (Printf.sprintf "\tret 8 * (2 + %d)\n" (List.length params'))
          ^ (Printf.sprintf "%s:\t; new closure is in rax\n" label_end)
       | ScmLambda' (params', Opt opt, body) ->  (*TODO Nadav: FROM chapter 6 slides: page 100 *)
-         raise X_not_yet_implemented
-      | ScmApplic' (proc, args, Non_Tail_Call) -> (*TODO Nadav: FROM chapter 6 slides: page 97 *) 
         raise X_not_yet_implemented
+      | ScmApplic' (proc, args, Non_Tail_Call) -> (* DONE *)
+        let reversed_args = List.rev args in
+        let per_arg_exps = String.concat "" (List.map (fun arg -> (run params env arg) ^ "\tpush rax\n") reversed_args)
+        in
+        per_arg_exps ^ 
+        Printf.sprintf "\tpush %i\n" (List.length args) ^
+        (run params env proc) ^
+        "\tassert_closure(rax)\n" ^
+        "\tSOB_CLOSURE_ENV(rax)\n" ^
+        "\tSOB_CLOSURE_CODE(rax)\n"
+
       | ScmApplic' (proc, args, Tail_Call) -> (*TODO Nadav: FROM chapter 6 slides: page 108 *)
         raise X_not_yet_implemented
     and runs params env exprs' =
