862f7e9ae12a30ef4eccc8f2d24f8ce3a1ac9ec7
MattanCohen <mc.3552883@gmail.com>
2023-01-25 23:40:21 +0200

    s

--- src/init.scm
+++ src/init.scm
@@ -1,3 +1,5 @@
+;;; init.scm
+;;; Initial definitions that should be available for the compiler
 
 (define (caar x) (car (car x)))
 (define (cadr x) (car (cdr x)))
@@ -40,7 +42,16 @@
 (define (fraction? q)
   (and (rational? q)
        (not (integer? q))))
+(define list*-run (lambda (a s)
+               (if (null? s)
+                   a
+                   (cons a
+                     (run (car s) (cdr s))))))
 
+(define list*
+  (let ((run list*-run))
+    (lambda (a . s)
+      (run a s))))
 
 (define throw-error (lambda () (error 'some-bin-func "bad numbers bro come'on")))
 
@@ -345,20 +356,6 @@
   )
 )
 
-; (define make-list
-;   (lambda (n . chs)
-;     (cond ((null? chs) (make-list-internal n #\nul))
-;             ((and (pair? chs)
-;                   (null? (cdr chs))
-;                   (char? (car chs)))
-;              (make-list-internal n (car chs)))
-;             (else (error 'make-list
-;                     "Usage: (make-list length ?optional-init-char)"))))
-;)
-
-; make-list
-
-
 (define char<? #void)
 (define char<=? #void)
 (define char=? #void)
@@ -441,141 +438,220 @@
 
 
 
-; (define make-string-run
-;        (lambda (i str1 len1 str2 len2)
-;               (or (and (= i len1) (< len1 len2))
-;               (and
-;                      (< i len1)
-;                      (or (char<?
-;                      (string-ref str1 i)
-;                      (string-ref str2 i))
-;                      (and (char=?
-;                             (string-ref str1 i)
-;                             (string-ref str2 i))
-;                             (make-string-run (+ i 1) str1 len1 str2 len2)))))))
+(define make-string-run
+       (lambda (i str1 len1 str2 len2)
+              (or (and (= i len1) (< len1 len2))
+              (and
+                     (< i len1)
+                     (or (char<?
+                     (string-ref str1 i)
+                     (string-ref str2 i))
+                     (and (char=?
+                            (string-ref str1 i)
+                            (string-ref str2 i))
+                            (make-string-run (+ i 1) str1 len1 str2 len2)))))))
        
 
-; (define make-string-run-bin
-;        (lambda (str strs)
-;        (or (null? strs)
-;               (and (binary-string<? str (car strs)) (make-string-run-bin (car strs) (cdr strs))))))
+(define make-string-run-bin
+       (lambda (str strs)
+       (or (null? strs)
+              (and (binary-string<? str (car strs)) (make-string-run-bin (car strs) (cdr strs))))))
 
-; (let ((make-string<?
-;         (lambda (char<? char=?)
-;           (letrec (run make-string-run)
-;             (let ((binary-string<?
-;                     (lambda (str1 str2)
-;                       (let ((len1 (string-length str1))
-;                             (len2 (string-length str2)))
-;                         (if (<= len1 len2)
-;                             (run 0 str1 len1 str2 len2)
-;                             (run 0 str2 len2 str1 len1))))))
-;               (letrec ((run make-string-run-bin))
-;                 (lambda (str . strs)
-;                   (run str strs))))))))
-;   (set! string<? (make-string<? char<? char=?))
-;   (set! string-ci<? (make-string<? char-ci<? char-ci=?))
-;   (set! string>? (make-string<? char>? char=?))
-;   (set! string-ci>? (make-string<? char-ci>? char-ci=?)))
-
-
-; (define make-string-run1 (lambda (i str1 len1 str2 len2)
-;                        (or (= i len1)
-;                            (char<?
-;                              (string-ref str1 i)
-;                              (string-ref str2 i))
-;                            (and (< i len1)
-;                                 (char=?
-;                                   (string-ref str1 i)
-;                                   (string-ref str2 i))
-;                                 (run (+ i 1) str1 len1 str2 len2)))))
-
-; (define make-string-run-bin1 (lambda (str strs)
-;                            (or (null? strs)
-;                                (and (binary-string<=? str (car strs)) (run (car strs) (cdr strs))))))
-
-; (let ((make-string<=?
-;         (lambda (char<? char=?)
-;           (let ((run make-string-run1))
-;             (let ((binary-string<=?
-;                     (lambda (str1 str2)
-;                       (let ((len1 (string-length str1))
-;                             (len2 (string-length str2)))
-;                         (if (<= len1 len2)
-;                             (run 0 str1 len1 str2 len2)
-;                             (run 0 str2 len2 str1 len1))))))
-;               (let ((run make-string-run-bin1))
-;                 (lambda (str . strs)
-;                   (run str strs))))))))
-;   (set! string<=? (make-string<=? char<? char=?))
-;   (set! string-ci<=? (make-string<=? char<? char=?))
-;   (set! string>=? (make-string<=? char>? char=?))
-;   (set! string-ci>=? (make-string<=? char-ci>? char-ci=?)))
-
-; (define make-string-run2 (lambda (i str1 str2 len)
-;                        (or (= i len)
-;                            (and (< i len)
-;                                 (char=?
-;                                   (string-ref str1 i)
-;                                   (string-ref str2 i))
-;                                 (run (+ i 1) str1 str2 len)))))
-
-; (define make-string-run-bin2 (lambda (str strs)
-;                            (or (null? strs)
-;                                (and (binary-string=? str (car strs)) (run (car strs) (cdr strs))))))
-
-; (let ((make-string=?
-;         (lambda (char=?)
-;           (let ((run make-string-run2))
-;             (let ((binary-string=?
-;                     (lambda (str1 str2)
-;                       (let ((len1 (string-length str1))
-;                             (len2 (string-length str2)))
-;                         (and (= len1 len2)
-;                              (run 0 str1 str2 len1))))))
-;               (let ((run make-string-run-bin2))
-;                 (lambda (str . strs)
-;                   (run str strs))))))))
-;   (set! string=? (make-string=? char=?))
-;   (set! string-ci=? (make-string=? char-ci=?)))
-
-; (define length
-;   (lambda (s)
-;     (if (null? s)
-;         0
-;         (+ 1 (length (cdr s))))))
+(let ((make-string<?
+        (lambda (char<? char=?)
+          (let ((run make-string-run))
+            (let ((binary-string<?
+                    (lambda (str1 str2)
+                      (let ((len1 (string-length str1))
+                            (len2 (string-length str2)))
+                        (if (<= len1 len2)
+                            (run 0 str1 len1 str2 len2)
+                            (run 0 str2 len2 str1 len1))))))
+              (let ((run make-string-run-bin))
+                (lambda (str . strs)
+                  (run str strs))))))))
+  (set! string<? (make-string<? char<? char=?))
+  (set! string-ci<? (make-string<? char-ci<? char-ci=?))
+  (set! string>? (make-string<? char>? char=?))
+  (set! string-ci>? (make-string<? char-ci>? char-ci=?)))
 
 
+(define make-string-run1 (lambda (i str1 len1 str2 len2)
+                       (or (= i len1)
+                           (char<?
+                             (string-ref str1 i)
+                             (string-ref str2 i))
+                           (and (< i len1)
+                                (char=?
+                                  (string-ref str1 i)
+                                  (string-ref str2 i))
+                                (run (+ i 1) str1 len1 str2 len2)))))
+
+(define make-string-run-bin1 (lambda (str strs)
+                           (or (null? strs)
+                               (and (binary-string<=? str (car strs)) (run (car strs) (cdr strs))))))
+
+(let ((make-string<=?
+        (lambda (char<? char=?)
+          (let ((run make-string-run1))
+            (let ((binary-string<=?
+                    (lambda (str1 str2)
+                      (let ((len1 (string-length str1))
+                            (len2 (string-length str2)))
+                        (if (<= len1 len2)
+                            (run 0 str1 len1 str2 len2)
+                            (run 0 str2 len2 str1 len1))))))
+              (let ((run make-string-run-bin1))
+                (lambda (str . strs)
+                  (run str strs))))))))
+  (set! string<=? (make-string<=? char<? char=?))
+  (set! string-ci<=? (make-string<=? char<? char=?))
+  (set! string>=? (make-string<=? char>? char=?))
+  (set! string-ci>=? (make-string<=? char-ci>? char-ci=?)))
+
+(define make-string-run2 (lambda (i str1 str2 len)
+                       (or (= i len)
+                           (and (< i len)
+                                (char=?
+                                  (string-ref str1 i)
+                                  (string-ref str2 i))
+                                (run (+ i 1) str1 str2 len)))))
+
+(define make-string-run-bin2 (lambda (str strs)
+                           (or (null? strs)
+                               (and (binary-string=? str (car strs)) (run (car strs) (cdr strs))))))
+
+(let ((make-string=?
+        (lambda (char=?)
+          (let ((run make-string-run2))
+            (let ((binary-string=?
+                    (lambda (str1 str2)
+                      (let ((len1 (string-length str1))
+                            (len2 (string-length str2)))
+                        (and (= len1 len2)
+                             (run 0 str1 str2 len1))))))
+              (let ((run make-string-run-bin2))
+                (lambda (str . strs)
+                  (run str strs))))))))
+  (set! string=? (make-string=? char=?))
+  (set! string-ci=? (make-string=? char-ci=?)))
+
+(define length
+  (lambda (s)
+    (if (null? s)
+        0
+        (+ 1 (length (cdr s))))))
+
+(define list?
+  (lambda (e)
+    (or (null? e)
+        (and (pair? e)
+             (list? (cdr e))))))
+
+(define make-vector
+  (let ((asm-make-vector make-vector))
+    (lambda (n . xs)
+      (let ((x
+              (cond ((null? xs) #void)
+                    ((and (pair? xs)
+                          (null? (cdr xs)))
+                     (car xs))
+                    (else (error 'make-vector
+                            "Usage: (make-vector size ?optional-default)")))))
+        (asm-make-vector n x)))))
+
+(define make-string
+  (let ((asm-make-string make-string))
+    (lambda (n . chs)
+      (let ((ch
+              (cond ((null? chs) 'nul)  ; TODO : cant use #\nul
+                    ((and (pair? chs)
+                          (null? (cdr chs)))
+                     (car chs))
+                    (else (error 'make-string
+                            "Usage: (make-string size ?optional-default)")))))
+        (asm-make-string n ch)))))
+
+(define list->vector-run (lambda (s i)
+               (if (null? s)
+                   (make-vector i #void)
+                   (let ((v (run (cdr s) (+ i 1))))
+                     (vector-set! v i (car s))
+                     v))))
+
+(define list->vector
+  (let ((run list->vector-run))
+    (lambda (s)
+      (run s 0))))
+
+(define list->string-run (lambda (s i)
+               (if (null? s)
+                   (make-string i 'nul)  ; TODO : cant use #\nul
+                   (let ((str (run (cdr s) (+ i 1))))
+                     (string-set! str i (car s))
+                     str))))
+
+(define list->string
+  (let ((run make->string-run))
+    (lambda (s)
+      (run s 0))))
+      
+(define vector (lambda s (list->vector s)))
+
+(define string->list-run (lambda (str i n)
+               (if (< i n)
+                   (cons (string-ref str i)
+                     (run str (+ i 1) n))
+                   '())))
+
+(define string->list
+  (let ((run string->list-run))
+    (lambda (str)
+      (run str 0 (string-length str)))))
+
+(define vector->list-run (lambda (v i n)
+               (if (< i n)
+                   (cons (vector-ref v i)
+                     (run v (+ i 1) n))
+                   '())))
+
+(define vector->list
+  (let ((run vector->list-run))
+    (lambda (v)
+      (run v 0 (vector-length v)))))
+
+(define random (lambda (n) (remainder (trng) n)))
+
+(define positive?
+  (lambda (x)
+    (< 0 x)))
+
+(define negative? (lambda (x) (< x 0)))
+
+(define even? (lambda (n) (zero? (remainder n 2))))
+
+(define odd? (lambda (n) (not (even? n))))
+
+(define abs (lambda (x) (if (negative? x) (- x) x)))
 
 
-; (define positive?
-;   (lambda (x)
-;     (< 0 x)))
+(define equal?
+  (lambda (e1 e2)
+    (cond ((and (pair? e1) (pair? e2))
+           (and (equal? (car e1) (car e2))
+                (equal? (cdr e1) (cdr e2))))
+          ((and (vector? e1) (vector? e2)
+                (= (vector-length e1) (vector-length e2)))
+           (equal? (vector->list e1) (vector->list e2)))
+          ((and (string? e1) (string? e2)
+                (= (string-length e1) (string-length e2)))
+           (string=? e1 e2))
+          (else (eq? e1 e2)))))
 
-; (define negative? (lambda (x) (< x 0)))
+(define assoc
+  (lambda (a s)
+    (cond ((null? s) #f)
+          ((eq? (caar s) a) (car s))
+          (else (assoc a (cdr s))))))
 
-; (define even? (lambda (n) (zero? (remainder n 2))))
-
-; (define odd? (lambda (n) (not (even? n))))
-
-; (define abs (lambda (x) (if (negative? x) (- x) x)))
-
-
-; (define equal?
-;   (lambda (e1 e2)
-;     (cond ((and (pair? e1) (pair? e2))
-;            (and (equal? (car e1) (car e2))
-;                 (equal? (cdr e1) (cdr e2))))
-;           ((and (vector? e1) (vector? e2)
-;                 (= (vector-length e1) (vector-length e2)))
-;            (equal? (vector->list e1) (vector->list e2)))
-;           ((and (string? e1) (string? e2)
-;                 (= (string-length e1) (string-length e2)))
-;            (string=? e1 e2))
-;           (else (eq? e1 e2)))))
-
-; (define assoc
-;   (lambda (a s)
-;     (cond ((null? s) #f)
-;           ((eq? (caar s) a) (car s))
-;           (else (assoc a (cdr s))))))
+;;; end-of-input
--- src/scheme_files/src/test1.scm
+++ src/scheme_files/src/test1.scm
@@ -1,19 +1,55 @@
-; (define test
-;     (lambda (x . s) s)
+(andmap number? 1 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; (define lambda-variadic ((lambda a (quasiquote (variadic lambda on list ,@a))) 1 2))
+
+; (define lambda-opt ((lambda (a . b) (quasiquote (lambda opt arg ,a rest ,@b))) 6 9))
+
+; (define plus ((lambda (a b) (quasiquote (binary plus performing ,a + ,b result is ,(bin+ a b)))) 5 4))
+
+; (define set-example (let ((x 2)) (begin (set! x 3) (quasiquote (set bang x was 2 set to ,x)))))    
+; (define set-example-in-lambda (lambda () (define nadav 0) (set! nadav (quasiquote (set bang in lambda works))) nadav))
+
+; (define sprint-list (lambda (l) (if (null? l) (quasiquote (letrec macro expansion works)) (sprint-list (cdr l)))))  
+; (define letrec-no-let (sprint-list '(1 2 3)))
+
+; (define fact (lambda (n) (if (zero? n) 1 (bin* n (fact (bin- n 1))))))
+; (define fact-example (quasiquote (fact of 3 ,(fact 3))))
+
+; (let ((should-test #f)
+;     (divider '_____________________________________________________________________________________________________________________________________________) (space '______))
+; (if (not should-test)   
+;         '()
+;         (quasiquote (
+;             ,divider
+;             ,lambda-variadic ,space
+;             ,lambda-opt ,space
+;             ,plus ,space
+;             ,fact-example ,space
+;             ,set-example ,space
+;             ,(set-example-in-lambda) ,space
+;             ,letrec-no-let
+;             )))
 ; )
 
-; (test 1 2 3)
-
-(define inc (lambda (x) (+ x 1)))
-(define id (lambda (x) x))
-
-; (andmap id #t #f 2)
-; (reverse '(1 2 3))
 
 
-; (map inc 1 2)
-
-; (prepand '(1 2 3) 5)
-
-(fold-left - 3 2 1)
-
+; ******    letrec recursions don't work
+; (define letrec-example (lambda ()  
+;     (letrec ((sprint-list (lambda (l) (if (null? l) (quasiquote (letrec works)) (sprint-list (cdr l)))))) (sprint-list '(1 2 3)))))
+; (letrec-example) ; supposed to print "letrec works" but instead throws exception  *********
